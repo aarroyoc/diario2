f3b01778d6b9f462783229bed4a07970b808f742
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2023-06-01 16:07:54 +0200

    Connect to PostgreSQL

--- justfile
+++ justfile
@@ -1,2 +1,10 @@
+default:
+	echo "Not available yet"
+
 psql-up:
 	docker-compose up postgres -d
+
+get-deps:
+	rm -rf postgresql-prolog
+	git clone --depth 1 --branch v1.0.0 https://github.com/aarroyoc/postgresql-prolog
+	
--- main.pl
+++ main.pl
@@ -1,7 +1,16 @@
+:- use_module(library(http/http_server)).
 :- use_module(library(format)).
+
+:- use_module(handlers/post).
+
+:- use_module('../postgresql-prolog/postgresql').
 
 :- initialization(main).
 
 main :-
     portray_clause(diario(2.0, "Adrián Arroyo Calle")),
+    connect("postgres", "postgres", '127.0.0.1', 5432, "postgres", Connection),
+    http_listen(9099, [
+        get(Slug, post_handler(Connection, Slug))
+    ]),
     halt.
